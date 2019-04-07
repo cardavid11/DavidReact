0c301e1d0db86d6df98e0c0fce065631bce8ad97
cardavid11 <34225554+cardavid11@users.noreply.github.com>
2019-04-07 15:39:14 -0400

    tarea S1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+class DishDetail extends Component {
+
+    renderDish(dish) {
+        if (dish != null)
+            return (
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
+
+    renderComments(dishSource) {
+        if (dishSource == null)
+            return (<div></div>);
+        var result = [];
+        const commentResult = dishSource.comments.map((comment) => {
+            console.log(comment);
+            return (
+                <div id={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>--{comment.author} , {comment.date}</p>
+                </div>
+            );
+        });
+        result.push(<h4> Comments </h4>);
+        result.push(commentResult);
+        return (result);
+    }
+
+    render() {
+
+        return (
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.dish)}
+                </div>
+            </div>
+        );
+    }
+}
+export default DishDetail;
